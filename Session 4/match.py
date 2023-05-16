dbf7aef839e67b16bd012e36583abb641f7a3683
Alejandro <avelez@zilliant.com>
2023-05-16 18:54:44 -0500

    agregado match con dict

--- Session 4/match.py
+++ Session 4/match.py
@@ -1,3 +1,4 @@
+# Usar python version >= 3.10
 
 def funcion_desde_case():
     print("ENTRE a funcion desde case")
@@ -15,3 +16,17 @@ def funcion_con_match(code):
 
 
 funcion_con_match(400)
+
+
+def funcion_con_dict(code):
+    diccionario = {
+        300: "Entre a 300",
+        200: "Entre a 200",
+        400: "Entre a 400",
+    }
+    return diccionario[code]
+
+try:
+    print(funcion_con_dict(300))
+except ValueError:
+    print("Case por defecto")
